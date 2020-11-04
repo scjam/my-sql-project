4910dc60ad8e00c792e141811c7ce070ec5f024a
Sydney James <sydneyjames7@gmail.com>
2020-11-04 15:21:52 -0800

    switched directors to separate table

---
+++ data/directors.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: 'James Cameron'
+  },
+  {
+    name: 'Anthony Minghella'
+  },
+  {
+    name: 'Jonathan Demme'
+  },
+  {
+    name: 'Quentin Tarantino'
+  },
+  {
+    name: 'Sofia Coppola'
+  }
+];
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-console */
 const client = require('../lib/client');
-// import our seed data:
 const movies = require('./movies.js');
+const directors = require('./directors.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
 run();
-
 async function run() {
 
   try {
@@ -22,16 +21,27 @@ async function run() {
         [user.email, user.hash]);
       })
     );
-      
+    
+    await Promise.all(
+      directors.map(director => {
+        return client.query(`
+          INSERT INTO directors (name)
+          VALUES ($1)
+          RETURNING *;
+          `,
+        [director.name]);
+      })
+    );
+    
     const user = users[0].rows[0];
 
     await Promise.all(
       movies.map(movie => {
         return client.query(`
-                    INSERT INTO movies (name, year_released, best_picture_winner, director, owner_id)
+                    INSERT INTO movies (name, year_released, best_picture_winner, director_id, owner_id)
                     VALUES ($1, $2, $3, $4, $5);
                 `,
-        [movie.name, movie.year_released, movie.best_picture_winner, movie.director, user.id]);
+        [movie.name, movie.year_released, movie.best_picture_winner, movie.director_id, user.id]);
       })
     );
     
